147e24121feb4e5448aa96ed96206f11875d2a9e
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-08-27 18:03:54 -0300

    Using Observables.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -10,7 +12,7 @@ export class HeroService {
 
   constructor() { }
 
-  public getHeroes(): Hero[] {
-    return HEROES;
+  public getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
