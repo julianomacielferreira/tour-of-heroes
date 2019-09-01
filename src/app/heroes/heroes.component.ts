aeeba6068389407d98ab94b9a295171fb1b4f94f
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-01 15:15:38 -0300

    Remove dead code.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -21,10 +20,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelectHero(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
