0c206769b5b785f88d50d092c05a1421ca33b587
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-01 13:56:23 -0300

    Add dahsboard route.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,10 +15,10 @@ export class DashboardComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
-
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 
