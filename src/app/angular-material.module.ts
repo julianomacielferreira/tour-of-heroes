bef176effca36b55a58e5b02e7393f5e018c0803
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-06 01:51:31 -0300

    Remove redundant code.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
+const MaterialModules = [
+  MatButtonModule
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    MatButtonModule
-  ],
-  exports: [
-    MatButtonModule
-  ]
+  imports: [MaterialModules],
+  exports: [MaterialModules]
 })
 export class AngularMaterialModule { }
