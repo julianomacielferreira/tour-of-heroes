3f764c0e8c4d00afa14a8ef4592c7808d7423ecd
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-08 03:46:10 -0300

    Using material components.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider';
 
 const MaterialModules = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatGridListModule,
+  MatDividerModule
 ];
 
 @NgModule({
