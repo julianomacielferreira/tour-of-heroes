ba5787c7da18a444bbc1e48954731c6e1da95454
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-09 04:43:03 -0300

    Add angular material table.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -10,6 +10,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatLineModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 const MaterialModules = [
   MatButtonModule,
@@ -22,7 +24,9 @@ const MaterialModules = [
   MatDividerModule,
   MatListModule,
   MatLineModule,
-  MatTooltipModule
+  MatTooltipModule,
+  MatTableModule,
+  MatSortModule
 ];
 
 @NgModule({
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-heroes',
@@ -11,15 +12,23 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
+  tableColumns: string[] = ['id', 'name', 'action'];
+  dataSource = new MatTableDataSource([]);
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
+    this.dataSource.sort = this.sort;
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+      this.dataSource.data = heroes;
+    });
   }
 
   add(name: string): void {
@@ -32,6 +41,7 @@ export class HeroesComponent implements OnInit {
 
     this.heroService.addHero({ name } as Hero).subscribe(hero => {
       this.heroes.push(hero);
+      this.dataSource.data = this.heroes;
     });
   }
 
@@ -39,5 +49,6 @@ export class HeroesComponent implements OnInit {
 
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
+    this.dataSource.data = this.heroes;
   }
 }
