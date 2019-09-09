de37c2ae514fea13c2e31a4d23e6af01479d1fc0
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-09 00:01:37 -0300

    Using material list module.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -6,6 +6,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatLineModule } from '@angular/material/core';
 
 const MaterialModules = [
   MatButtonModule,
@@ -14,7 +16,9 @@ const MaterialModules = [
   MatFormFieldModule,
   MatCardModule,
   MatGridListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatListModule,
+  MatLineModule
 ];
 
 @NgModule({
